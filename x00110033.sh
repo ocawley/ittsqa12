41c3e266ec73130683a09572f2ee1622589cf3a0
Zamirk <x00110033@gmail.com>
2014-10-28 15:20:57 +0000

    Create x00110033.sh

---
+++ x00110033.sh
@@ -0,0 +1,12 @@
+java grades 50 48
+java grades 10 45  
+java grades 80 38
+java grades 60 60
+java grades 90 90
+java grades 120 120
+
+##Expected output pass
+##GLitch Detected
+java grades 55 60
+
+sleep 5m
